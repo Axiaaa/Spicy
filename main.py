f0fa5393a63d5662fd669a2563e4697c9a68a462
Nephtys <91245292+Axiaaa@users.noreply.github.com>
2024-03-10 06:56:16 +0100

    Initial commit
    Basics commands with basics error management are working

---
+++ main.py
@@ -0,0 +1,145 @@
+from interactions import *
+from requests import get
+from random import randint
+
+FOOTER_TEXT = "La question peut parfois être en anglais ou mal traduite, désolé pour la gêne occasionée !"
+
+async def api_request(str, ctx : InteractionContext):
+    try:
+        r = get("https://api.truthordarebot.xyz/v1/" + str, params="rating=R")
+        if r.status_code == 200:
+            return r.json()
+        else :
+            await ctx.send(f"Erreur avec la requete API", ephemeral=True)
+    except Exception as e:
+        await ctx.send(f"Erreur ! {e}", ephemeral=True)
+
+@listen(events.Ready)
+async def on_ready(event) -> None :
+    print("Je suis pret !")
+
+
+@listen(events.Startup)
+async def on_start(event) -> None:
+    print("Je demarre !")
+
+
+@slash_command(
+    name="verite",
+    description="Propose une vérité",
+    nsfw=True,
+    dm_permission=False
+    #TODO COOLDOWN
+)
+async def truth(ctx : InteractionContext) :
+    r = await api_request("truth", ctx)
+    if (r): 
+        embed = Embed(
+            title="Vérité",
+            color= (237, 127, 16),
+            footer=EmbedFooter(FOOTER_TEXT),
+            author=EmbedAuthor(ctx.author.display_name, icon_url=ctx.author.avatar_url)
+        )
+        if (r['translations']['fr']):
+            embed.description = r['translations']['fr']
+            embed.add_field(name="Anglais", value=r['question'], inline=False)
+        else :
+            embed.description = f"{r['question']}\n:warning: Cette question n'a pas pu être traduite.";
+        await ctx.send(embed=embed)
+
+@slash_command(
+    name="action",
+    description="Propose une action",
+    nsfw=True,
+    dm_permission=False
+    #TODO COOLDOWN
+)
+async def dare(ctx : InteractionContext):
+    r = api_request("dare")
+    if (r):
+        embed = Embed(
+            title="Action",
+            color=(127, 16, 237),
+            footer=EmbedFooter(FOOTER_TEXT),
+            author=EmbedAuthor(ctx.author.display_name, icon_url=ctx.author.avatar_url)  
+        )
+        if (r['translations']['fr']):
+            embed.description = r['translations']['fr']
+            embed.add_field(name="Anglais", value=r['question'], inline=False)
+        else :
+            embed.description = f"{r['question']}\n:warning: Cette question n'a pas pu être traduite.";
+        await ctx.send(embed=embed)
+
+@slash_command(
+    name="dilemme",
+    description="Propose un dilemme sous forme de \"Tu preferes\"",
+    nsfw=True,
+    dm_permission=False
+    #TODO COOLDOWN
+)
+async def wyr(ctx : InteractionContext):
+    r = api_request("wyr")
+    if (r):
+        embed = Embed(
+            title="Tu péfères ?",
+            color=(255, 255, 0),
+            footer=EmbedFooter(FOOTER_TEXT),
+            author=EmbedAuthor(ctx.author.display_name, icon_url=ctx.author.avatar_url)  
+        )
+        if (r['translations']['fr']):
+            embed.description = r['translations']['fr']
+            embed.add_field(name="Anglais", value=r['question'], inline=False)
+        else :
+            embed.description = f"{r['question']}\n:warning: Ce dilemme n'a pas pu être traduit.";
+        await ctx.send(embed=embed)
+
+@slash_command(
+    name="n_as_tu_jamais",
+    description="Propose une sous forme de \"N'as-tu jamais ...\"",
+    nsfw=True,
+    dm_permission=False
+    #TODO COOLDOWN
+)
+async def nhie(ctx : InteractionContext):
+    r = api_request("nhie")
+    if (r):
+        embed = Embed(
+            title="N'as-tu jamais ?",
+            color=(255, 0, 0),
+            footer=EmbedFooter(FOOTER_TEXT),
+            author=EmbedAuthor(ctx.author.display_name, icon_url=ctx.author.avatar_url)  
+        )
+        if (r['translations']['fr']):
+            embed.description = r['translations']['fr']
+            embed.add_field(name="Anglais", value=r['question'], inline=False)
+        else :
+            embed.description = f"{r['question']}\n:warning: Cette question n'a pas pu être traduite.";
+        await ctx.send(embed=embed)
+
+
+@slash_command(
+    name="paranoia",
+    description="Pose une question pour te mettre mal à l'aise",
+    nsfw=True,
+    dm_permission=False
+    #TODO COOLDOWN
+)
+async def paranoia(ctx : InteractionContext):
+    r = api_request("paranoia")
+    if (r):
+        embed = Embed(
+            title="Paranoia",
+            color=(255, 0, 255),
+            footer=EmbedFooter(FOOTER_TEXT),
+            author=EmbedAuthor(ctx.author.display_name, icon_url=ctx.author.avatar_url)  
+        )
+        if (r['translations']['fr']):
+            embed.description = r['translations']['fr']
+            embed.add_field(name="Anglais", value=r['question'], inline=False)
+        else :
+            embed.description = f"{r['question']}\n:warning: Cette question n'a pas pu être traduite.";
+        await ctx.send(embed=embed)
+
+bot = Client(token="", intents=Intents.ALL)
+bot.start()
+
