e4ba65d1437994ff1962f2b6f48d1755454394ac
Nephtys <h.camerlynck@hotmail.com>
2024-03-30 11:24:25 +0100

    Minor change in the /reload import

--- exts/info.py
+++ exts/info.py
@@ -4,17 +4,17 @@ class Info(Extension):
      
     @slash_command(name="info", description="Affiche les informations du bot")
     async def info(self, ctx : InteractionContext):
-            try : 
-                embed = Embed(title="Informations du bot", color=Color.from_hex("#FF0000"))
-                embed.add_field(name="Développé en", value="Python, avec [interactions.py (v5)](https://github.com/interactions-py/interactions.py)", inline=True)
-                embed.add_field(name="Créé le", value=f"{self.bot.app.created_at}", inline=False)
-                embed.add_field(name="Créé par", value=f"<@{self.bot.owner.id}>", inline=False)
-                embed.add_field(name="Serveurs", value=len(self.bot.guilds), inline=True)
-                embed.add_field(name="Latence", value=f"{round(self.bot.latency * 100)} ms", inline=True)
-                embed.set_footer(text=self.bot.app.name, icon_url=self.bot.user.avatar_url)
-                await ctx.send(embed=embed)
-            except OverflowError : 
-                await ctx.send("La commande est indisponible pour le moment. Réessayes dans quelques minutes.", ephemeral=True)
+        try : 
+            embed = Embed(title="Informations du bot", color=Color.from_hex("#FF0000"))
+            embed.add_field(name="Développé en", value="Python, avec [interactions.py (v5)](https://github.com/interactions-py/interactions.py)", inline=True)
+            embed.add_field(name="Créé le", value=f"{self.bot.app.created_at}", inline=False)
+            embed.add_field(name="Créé par", value=f"<@{self.bot.owner.id}>", inline=False)
+            embed.add_field(name="Serveurs", value=len(self.bot.guilds), inline=True)
+            embed.add_field(name="Latence", value=f"{round(self.bot.latency * 100)} ms", inline=True)
+            embed.set_footer(text=self.bot.app.name, icon_url=self.bot.user.avatar_url)
+            await ctx.send(embed=embed)
+        except OverflowError : 
+            await ctx.send("La commande est indisponible pour le moment. Réessayes dans quelques minutes.", ephemeral=True)
 
 def setup(bot):
     Info(bot)
--- exts/reload.py
+++ exts/reload.py
@@ -1,14 +1,14 @@
-import interactions, os, logging
-from interactions import is_owner
+import os, logging
+from interactions import *
 
-class Reload(interactions.Extension): 
+class Reload(Extension): 
 
     is_owner()
-    @interactions.slash_command(
+    @slash_command(
             name="reload", 
             description="Cette commande permet de recharger les extensions du bot."
             )
-    async def reload(self, ctx : interactions.InteractionContext):
+    async def reload(self, ctx : InteractionContext):
         def reload_extensions(bot, folder, prefix="", exclude_files=[]):
             extensions = [file.replace(".py", "") for file in os.listdir(folder) if file.endswith(".py") and file not in exclude_files]
             for ext in extensions:
