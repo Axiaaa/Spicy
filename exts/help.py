fd5174dd903e2d4e2385a29d92eed97172926b91
Nephtys <lcamerly@student.42lyon.fr>
2024-04-02 11:26:49 +0200

    Added /help content + /ping to get bot's ms faster than /info

--- exts/help.py
+++ exts/help.py
@@ -9,8 +9,16 @@ class Help(Extension):
     )
     async def help(self, ctx : InteractionContext):
         embed = Embed(
-            None
+            title="Liste des mes commandes",
+            description="Si vous voyez :underage: devant une commande, c'est qu'elle doit être éxécutée dans un salon NSFW",
         )
+        embed.add_field(name="`/help`", value="Affiche cette liste", inline=False)
+        embed.add_field(name="`/info`", value="Affiche certaines informations sur le bot", inline=False)
+        embed.add_field(name="`/ping`", value="Ping le bot", inline=False)
+        embed.add_field(name=":underage: `/verite`", value="Propose une vérité", inline=False)
+        embed.add_field(name=":underage: `/action`", value="Propose un gage. Certaines actions marchent mieux si vous jouer avec vos amis autours de vous", inline=False)
+        embed.add_field(name=":underage: `/dilemme`", value="Propose un dilemme", inline=False)
+        embed.add_field(name=":underage: `/n_as_tu_jamais`", value="Propose une quesions sous forme de \"N'as tu jamais ...\" avec des réactions pour réagir", inline=False)
         await ctx.send(embed=embed)
 
 def setup(bot):
